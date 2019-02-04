ac59b2d230eada1dcab4b0ef45139ec8554a75b0
Samantha Wong <smiwong920@gmail.com>
2019-02-04 15:34:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  language_attributes = {}
+
+  languages.each do |oo_or_functional, language_hash|
+    language_hash.each do |language, attribute_hash|
+      attribute_hash.each do |attribute, str_value|
+        if language_attributes[language].nil?
+          #if language :ruby doesn't exist in new hash
+          language_attributes[language] = {}  #initiate a new hash for the value of :ruby
+        end
+        language_attributes[language][:style] ||= []  
+        language_attributes[language][:style] << oo_or_functional     
+        if language_attributes[language][attribute].nil?
+          language_attributes[language][attribute] = str_value  
+        end
+      end
+    end
+  end
+  language_attributes
 end
